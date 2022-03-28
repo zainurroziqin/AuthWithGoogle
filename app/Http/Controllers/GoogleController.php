b191cbc2163dd62beb4ece6ed86e96c06a857d2d
zainurroziqin <zainur7jan2002@gmail.com>
2022-03-28 08:58:59 +0700

    update

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback(Request $request)
+    {
+        try{
+            $user = Socialite::driver('google')->user();
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser){
+                Auth()->login($finduser);
+                return redirect()->intended('dashboard');
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id' => $user->id,
+                    'password' => encrypt('123456dummy')
+                ]);
+                Auth()->login($newUser);
+                return redirect()->intended('dashboard');
+            }
+        }catch(Exception $e){
+            dd($e->getMessage());
+        }   
+    }
+}
